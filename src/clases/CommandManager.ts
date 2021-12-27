a58918c7bf359991217bc27ed1c518664b0871d2
Elí Maciel <tecno_eli@hotmail.com>
2021-12-26 22:36:12 -0600

    Create CommandManager.ts

---
+++ src/clases/CommandManager.ts
@@ -0,0 +1,23 @@
+import fs from "fs";
+import { Collection, Guild } from "discord.js";
+import { Command, Client } from "../utils/clases";
+import {join} from "path"
+
+export class CommandManager extends Collection<string, Command> {
+    client: Client;
+
+    constructor(client: Client, path: string) {
+        super();
+        this.client = client;
+        for (const file of fs.readdirSync(path).filter((f) => f.endsWith(".command.js"))) {            
+            const command = require(join(path, file));
+            
+            const cmd: Command = new command.default(client);
+            this.set(cmd.name, cmd);
+        }
+    }
+    
+    deploy(guild?: Guild) {
+        return Promise.all(this.map((command) => command.deploy(guild)));
+    }
+}
