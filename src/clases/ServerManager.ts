0281268f914d52d8dc9546ef491d8466e7e90caf
Elí Maciel <tecno_eli@hotmail.com>
2021-12-26 22:36:21 -0600

    Create ServerManager.ts

---
+++ src/clases/ServerManager.ts
@@ -0,0 +1,15 @@
+import { Collection, Guild } from "discord.js";
+import { Server, Client } from "../utils/clases";
+
+export class ServerManager extends Collection<string, Server> {
+    client: Client;
+
+    constructor(client: Client) {
+        super();
+        this.client = client;
+    }
+    
+    initialize(guild?: Guild) {
+        return Promise.all(this.client.guilds?.cache.map((guild: Guild) => this.set(guild.id, new Server(guild))));
+    }
+}
