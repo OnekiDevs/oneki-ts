5bd9f5fbadaace163b7fab2c239c450a480250b9
eliyya <tecno_eli@hotmail.com>
2022-03-15 21:12:06 -0600

    command `/config set` destructured

--- src/commands/config/set.ts
+++ src/commands/config/set.ts
@@ -19,15 +19,33 @@ export function prefix(interaction: CommandInteraction<'cached'>) {
     if (!member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return permissionsError(interaction, Permissions.FLAGS.ADMINISTRATOR)
     const prefix = interaction.options.getString('prefix') as string
     server.setPrefix(prefix)
+    interaction.reply(server.translate('config_cmd.set_prefix', { prefix }))
     ;(interaction.client as Client).commands.get('config')?.deploy(interaction.guild) //TODO cambiar a autocompletado
 }
 
-export function suggest_channel(interaction: CommandInteraction<'cached'>) {
+export async function suggest_channel(interaction: CommandInteraction<'cached'>) {
     const member = interaction.guild?.members.cache.get(interaction.user.id)
     let server = (interaction.client as Client).servers.get(interaction.guildId)
     if (!server) server = newServer(interaction.guild)
     if (!member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return permissionsError(interaction, Permissions.FLAGS.ADMINISTRATOR)
     const channel = interaction.options.getChannel('channel') as TextChannel
     server.setSuggestChannel(channel)
+    interaction.reply(server.translate('config_cmd.set_suggest_channel.reply', { channel }))
+    try {
+        await channel.sendTyping()
+        channel.send(server.translate('config_cmd.set_suggest_channel.message'))
+    } catch (error) {
+        //TODO manejar este error
+    }
     ;(interaction.client as Client).commands.get('config')?.deploy(interaction.guild) //TODO cambiar a autocompletado
+}
+
+export function birthday_channel(interaction: CommandInteraction<'cached'>) {
+    const member = interaction.guild?.members.cache.get(interaction.user.id)
+    let server = (interaction.client as Client).servers.get(interaction.guildId)
+    if (!server) server = newServer(interaction.guild)
+    if (!member?.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) return permissionsError(interaction, Permissions.FLAGS.ADMINISTRATOR)
+    const birthdayChannel = interaction.options.getChannel('channel') as TextChannel
+    server.setBirthdayChannel(birthdayChannel.id)
+    interaction.reply(server.translate('config_cmd.set_birthday', { channel: birthdayChannel?.toString() }))
 }
