8d29b2ceccf236b809947b671e9e9917ada42303
Elí Maciel <tecno_eli@hotmail.com>
2022-03-22 23:00:10 -0600

    delete default response to interactions

--- src/events/interactionCreate.event.ts
+++ src/events/interactionCreate.event.ts
@@ -6,21 +6,8 @@ export const name = 'interactionCreate'
 
 export async function run(interaction: CommandInteraction | ButtonInteraction) {
     try {
-        if (interaction.isApplicationCommand()) {
-            //isApplicationCommand
-            if ((interaction.client as Client).commands.has(interaction.commandName)) {
-                (interaction.client as Client).commands.get(interaction.commandName)?.run(interaction)
-            } else {
-                interaction.reply({
-                    content: '`ctrl` + `R`',
-                    ephemeral: true
-                })
-            }
-        } else if (interaction.isButton()) {
-            const btn = await (interaction.client as Client).buttons.find(btn => btn.regex.test(interaction.customId))
-            if (btn) btn.run(interaction)
-            else interaction.deferUpdate()
-        }
+        if (interaction.isApplicationCommand()) (interaction.client as Client).commands.get(interaction.commandName)?.run(interaction)
+        else if (interaction.isButton()) (interaction.client as Client).buttons.find(btn => btn.regex.test(interaction.customId))?.run(interaction)
     } catch (error) {
         sendError(interaction.client as Client, error as Error, import.meta.url)
     }
