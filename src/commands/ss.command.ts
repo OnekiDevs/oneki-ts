17f7271e7b19ba7afdd8aab44ebd34da3460445f
Elí Maciel <tecno_eli@hotmail.com>
2022-05-01 20:30:45 -0500

    crop ss

--- src/commands/ss.command.ts
+++ src/commands/ss.command.ts
@@ -1,6 +1,7 @@
 import { ChatInputCommandInteraction, Attachment, UserFlagsBitField, ApplicationCommandOptionType } from 'discord.js'
 import { Command, Client } from '../utils/classes.js'
 import cw from 'capture-website'
+import Jimp from 'jimp'
 
 export default class SS extends Command {
     constructor(client: Client) {
@@ -39,11 +40,17 @@ export default class SS extends Command {
         if (member.user.bot) params.append('bot', '')
         if (member.user.flags?.has(UserFlagsBitField.Flags.VerifiedBot)) params.append('verified', '')
     
-        const ss = await cw.buffer('https://oneki.herokuapp.com/api/fake/discord/message?' + params, {
+        let ss = await cw.buffer('https://oneki.herokuapp.com/api/fake/discord/message?' + params, {
             height: Math.round((message.length * 50) / 140 + 50),
             width: 500,
             launchOptions: { args: ['--no-sandbox'] }
         })
+        ss = await Jimp.read(ss)
+        const base = ss.getPixelColor(0, 0)
+        ss.autocrop()
+        const c = new Jimp(ss.bitmap.width+20, ss.bitmap.height+20, base)
+        c.composite(ss, 10, 10)
+        ss = await c.getBufferAsync(Jimp.MIME_PNG)
         interaction.editReply({
             files: [new Attachment(ss, 'ss.jpg')]
         })
