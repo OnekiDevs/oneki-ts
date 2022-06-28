8e8dcd62962ed8174c174a40be288d8c2eb9ef67
tecno_eli@hotmail.com <eliyya>
2022-06-28 02:09:29 -0500

    reactivate text suggestions

--- src/events/messageCreate.event.ts
+++ src/events/messageCreate.event.ts
@@ -1,15 +1,15 @@
-import { PermissionsBitField, Message } from 'discord.js'
+import { PermissionsBitField, Message, EmbedBuilder, TextChannel } from 'discord.js'
 import { Client } from '../utils/classes.js'
-import { sendError } from '../utils/utils.js'
+import { sendError, Translator } from '../utils/utils.js'
 
 export default async function (msg: Message<true>) {
     const client = msg.client as Client
     const server = client.getServer(msg.guild)
+    const translate = Translator(msg)
     if (server.disabledChannels.includes(msg.channelId)) return //If it's a disabled channel
 
     try {
-        console.log(msg.attachments.size)
-
+        // attachments
         if (
             msg.attachments.size > 0 ||
             (msg.content.match(
@@ -20,6 +20,41 @@ export default async function (msg: Message<true>) {
                 ))
         )
             msg.client.emit('Attachment', msg)
+
+        // suggestions
+        const cs = server.suggestChannels.find(c => c.channel === msg.channelId)
+        if (cs) {
+            const channel = client.channels.cache.get(cs.channel) as TextChannel
+            if (channel) {
+                const embed = new EmbedBuilder()
+                    .setAuthor({
+                        name: msg.author.username,
+                        iconURL: msg.author.displayAvatarURL()
+                    })
+                    .setTitle(translate('suggest_cmd.title', { id: server?.lastSuggestId }))
+                    .setColor(16313844)
+                    .setDescription(msg.content)
+                    .setFooter(client.embedFooter)
+                    .setTimestamp()
+                channel
+                    .send({
+                        embeds: [embed]
+                    })
+                    .then(msg => {
+                        msg.startThread({
+                            name: translate('suggest_cmd.sent', { id: server?.lastSuggestId })
+                        })
+                        server.db.collection('suggests').doc(`suggest_${server.lastSuggestId}`).set({
+                            author: msg.author.id,
+                            channel: msg.channel.id,
+                            suggest: msg.content
+                        })
+                    })
+                server.lastSuggestId += 1
+            }
+        }
+
+        // commands
         const prefix = server?.prefixes.find(p => msg.content.startsWith(p))
         if (!prefix) return
         const args = msg.content.slice(prefix?.length).split(/ /gi)
