333a7feb6e794abb8a7d26b73cd6ff04197ef357
eliyya <tecno_eli@hotmail.com>
2022-11-02 16:56:47 -0600

    guildMemberRemove migrated

--- src/events/guild/guildMemberRemove.ts
+++ src/events/guild/guildMemberRemove.ts
@@ -1,16 +1,16 @@
 import { GuildMember } from 'discord.js'
-import client from '../client.js'
+import client from '../../client.js'
 
-export default function(member: GuildMember){
-    if(member.user.bot) return
+export default function (member: GuildMember) {
+    if (member.user.bot) return
 
     const server = client.getServer(member.guild)
 
-    if(!server.keepRoles || !server.premium) return
+    if (!server.keepRoles || !server.premium) return
 
     const memberRoles = member.roles.cache.filter(role => !role.managed && role.id !== member.guild.id)
     const memberIdRoles = memberRoles.map(role => role.id)
-    
+
     server.db.collection('users').doc(member.id).set({
         roles: memberIdRoles
     })
